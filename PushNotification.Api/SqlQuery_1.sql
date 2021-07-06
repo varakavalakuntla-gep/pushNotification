4de53baf9be38b75db99a33e7ec3f0c11364a4ea
raghavvirmani-gep <Raghav_Virmani@gep.com>
2021-07-06 15:46:00 +0530

    added frontend

--- PushNotification.Api/SqlQuery_1.sql
+++ PushNotification.Api/SqlQuery_1.sql
@@ -13,3 +13,16 @@ WITH VALUES;
 
 
 update dbo.notifications set readyn = 0 where notification_id in (22,24,26)
+
+INSERT INTO trash SELECT * FROM notifications WHERE AddedOn < DATEADD(day, -15, GETDATE());
+DELETE FROM notifications WHERE AddedOn < DATEADD(day, -15, GETDATE());
+select * from notifications;
+select * from trash;
+
+SET IDENTITY_INSERT notifications ON
+INSERT INTO notifications (notification_id,title,body,category,AddedOn,readyn,delivered,deleted) SELECT * FROM trash WHERE notification_id is NOT NULL;
+DELETE FROM trash WHERE notification_id is NOT NULL;
+SET IDENTITY_INSERT notifications OFF
+
+select * from categories;
+delete from notifications where category=5;
